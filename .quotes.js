276c1ea54ab4de829fa29f7b3c1147e812ac53ce
Jon Mortimer <86986753+whoIsThatRat@users.noreply.github.com>
2021-07-08 15:58:42 -0700

    Update .quotes.js

--- .quotes.js
+++ .quotes.js
@@ -1,1 +1,110 @@
-var a = 2;
+var quotes = {
+  albert_einstein: [
+    "We cannot solve our problems with the same thinking we used when we created them."
+  ],
+  marilyn_monroe: [
+    
+  ],
+  eleanor_roosevelt: [
+    
+  ],
+  will_rogers: [
+    
+  ],
+  william_blake: [
+    
+  ],
+  marcus_valerius_martialis: [
+    
+  ],
+  william_shakespeare: [
+    
+  ],
+  francois_marie_arouet: [
+    
+  ],
+  michel_de_montaigne: [
+    
+  ],
+  mark_twain: [
+    
+  ],
+  oscar_wilde: [
+    
+  ],
+  sappho: [
+    
+  ],
+  woody_allen: [
+    
+  ],
+  anacreon: [
+    
+  ],
+  aristotle: [
+    
+  ],
+  yogi_berra: [
+    
+  ],
+  buddha: [
+    
+  ],
+  george_carlin: [
+    
+  ],
+  winston_churchill: [
+    
+  ],
+  dante_alighieri: [
+    
+  ],
+  sigmund_freud: [
+    
+  ],
+  mahatma_gandhi: [
+    
+  ],
+  jesus: [
+    
+  ],
+  john_kennedy: [
+    
+  ],
+  martin_king: [
+    
+  ],
+  abraham_lincoln: [
+    
+  ],
+  groucho_marx: [
+    
+  ],
+  michelangelo: [
+    
+  ],
+  napoleon_bonaparte: [
+    
+  ],
+  plato: [
+    
+  ],
+  dorothy_parker: [
+    
+  ],
+  dolly_parton: [
+    
+  ],
+  socrates: [
+    
+  ],
+  jonathan_swift: [
+    
+  ],
+  mother_teresa: [
+    
+  ],
+  mae_west: [
+    
+  ],
+};
