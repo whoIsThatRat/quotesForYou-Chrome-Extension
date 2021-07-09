f8c61ed7c2fe721cafcd8a6b11ae861063b41e14
Jon Mortimer <86986753+whoIsThatRat@users.noreply.github.com>
2021-07-09 11:55:28 -0700

    Update 3_quotes.js

--- 3_quotes.js
+++ 3_quotes.js
@@ -1,110 +1,254 @@
 var quotes = {
-  albert_einstein: [
-    "We cannot solve our problems with the same thinking we used when we created them."
-  ],
-  marilyn_monroe: [
-    
-  ],
-  eleanor_roosevelt: [
-    
-  ],
-  will_rogers: [
-    
-  ],
-  william_blake: [
-    
-  ],
-  marcus_valerius_martialis: [
-    
-  ],
-  william_shakespeare: [
-    
-  ],
-  francois_marie_arouet: [
-    
-  ],
-  michel_de_montaigne: [
-    
-  ],
-  mark_twain: [
-    
-  ],
-  oscar_wilde: [
-    
-  ],
-  sappho: [
-    
-  ],
-  woody_allen: [
-    
-  ],
-  anacreon: [
-    
-  ],
-  aristotle: [
-    
-  ],
-  yogi_berra: [
-    
-  ],
-  buddha: [
-    
-  ],
-  george_carlin: [
-    
-  ],
-  winston_churchill: [
-    
-  ],
-  dante_alighieri: [
-    
-  ],
-  sigmund_freud: [
-    
-  ],
-  mahatma_gandhi: [
-    
-  ],
-  jesus: [
-    
-  ],
-  john_kennedy: [
-    
-  ],
-  martin_king: [
-    
-  ],
-  abraham_lincoln: [
-    
-  ],
-  groucho_marx: [
-    
-  ],
-  michelangelo: [
-    
-  ],
-  napoleon_bonaparte: [
-    
-  ],
-  plato: [
-    
-  ],
-  dorothy_parker: [
-    
-  ],
-  dolly_parton: [
-    
-  ],
-  socrates: [
-    
-  ],
-  jonathan_swift: [
-    
-  ],
-  mother_teresa: [
-    
-  ],
-  mae_west: [
-    
-  ],
+  albert_einstein: {
+    author: "Albert Einstein",
+    quotes: [
+      "We cannot solve our problems with the same thinking we used when we created them."
+    ]  
+  },
+  
+  marilyn_monroe: {
+    author: "Mariyln Monroe",
+    quotes: [
+      
+    ]  
+  },
+  
+  eleanor_roosevelt: {
+    author: "Eleanor Roosevelt",
+    quotes: [
+      
+    ]  
+  },
+  
+  will_rogers: {
+    author: "Will Rogers",
+    quotes: [
+      
+    ]  
+  },
+  
+  william_blake: {
+    author: "William Blake",
+    quotes: [
+      
+    ]  
+  },
+  
+  marcus_valerius_martialis: {
+    author: "Marcus Valerius Martialis",
+    quotes: [
+      
+    ]  
+  },
+  
+  william_shakespeare: {
+    author: "William Shakespeare",
+    quotes: [
+      
+    ]  
+  },
+  
+  francois_marie_arouet: {
+    author: "François-Marie Arouet",
+    quotes: [
+      
+    ]  
+  },
+  
+  michel_de_montaigne: {
+    author: "Michel Eyquem de Montaigne",
+    quotes: [
+      
+    ]  
+  },
+  
+  mark_twain: {
+    author: "Mark Twain",
+    quotes: [
+      
+    ]  
+  },
+  
+  oscar_wilde: {
+    author: "Oscar Wilde",
+    quotes: [
+      
+    ]  
+  },
+  
+  sappho: {
+    author: "Sappho",
+    quotes: [
+      
+    ]  
+  },
+  
+  woody_allen: {
+    author: "Woody Allen",
+    quotes: [
+      
+    ]  
+  },
+  
+  anacreon: {
+    author: "Anacreon",
+    quotes: [
+      
+    ]  
+  },
+  
+  aristotle: {
+    author: "Aristotle",
+    quotes: [
+      
+    ]  
+  },
+  
+  yogi_berra: {
+    author: "Yogi Berra",
+    quotes: [
+      
+    ]  
+  },
+  
+  buddha: {
+    author: "Buddha",
+    quotes: [
+      
+    ]  
+  },
+  
+  george_carlin: {
+    author: "George Carlin",
+    quotes: [
+      
+    ]  
+  },
+  
+  winston_churchill: {
+    author: "Winston Chruchill",
+    quotes: [
+      
+    ]  
+  },
+  
+  dante_alighieri: {
+    author: "Dante Alighieri",
+    quotes: [
+      
+    ]  
+  },
+  
+  sigmund_freud: {
+    author: "Sigmund Freud",
+    quotes: [
+      
+    ]  
+  },
+  
+  mahatma_gandhi: {
+    author: "Mahatma Gandhi",
+    quotes: [
+      
+    ]  
+  },
+  
+  jesus: {
+    author: "Jesus",
+    quotes: [
+      
+    ]  
+  },
+  
+  john_kennedy: {
+    author: "John F. Kennedy",
+    quotes: [
+      
+    ]  
+  },
+  
+  martin_king: {
+    author: "Martin Luther King Jr.",
+    quotes: [
+      
+    ]  
+  },
+  
+  abraham_lincoln: {
+    author: "Abraham Lincoln",
+    quotes: [
+      
+    ]  
+  },
+  
+  groucho_marx: {
+    author: "Groucho Marx",
+    quotes: [
+      
+    ]  
+  },
+  
+  michelangelo: {
+    author: "Michelangelo",
+    quotes: [
+      
+    ]  
+  },
+  
+  napoleon_bonaparte: {
+    author: "Napoléon Bonaparte",
+    quotes: [
+      
+    ]  
+  },
+  
+  plato: {
+    author: "Plato",
+    quotes: [
+      
+    ]  
+  },
+  
+  dorothy_parker: {
+    author: "Dorothy Parker",
+    quotes: [
+      
+    ]  
+  },
+  
+  dolly_parton: {
+    author: "Dolly Parton",
+    quotes: [
+      
+    ]  
+  },
+  
+  socrates: {
+    author: "Socrates",
+    quotes: [
+      
+    ]  
+  },
+  
+  jonathan_swift: {
+    author: "Jonathan Swift",
+    quotes: [
+      
+    ]  
+  },
+  
+  mother_teresa: {
+    author: "Mother Teresa",
+    quotes: [
+      
+    ]  
+  },
+  
+  mae_west: {
+    author: "Mae West",
+    quotes: [
+      
+    ]  
+  },
+  
 };
